78ef0a504cfc5caa955ced696e774a3dcf7e1470
Troy Harris <troyh.harris@deliveroo.co.uk>
2020-08-18 20:57:32 +0100

    refactor: use bool instead of result counter. split concerns via additional method

--- is_rotation.rb
+++ is_rotation.rb
@@ -2,22 +2,29 @@
 class Rotate
 
   def is_rotation(arrayA, arrayB)
-     index2, result = 0, 0
      a_size, b_size = arrayA.size, arrayB.size
 
      return false if a_size != b_size
-     
-     arrayA.each do |a|
-       until a == arrayB[index2]
-         index2 += 1
-         return false if index2 > b_size
-       end
-
-      index2 = (index2 + 1) % b_size # modulo magic
-      result += 1 
+     index = find_start_index(arrayA[0], arrayB)
+    
+     arrayA.each do|a|
+      return false if a != arrayB[index]
+      index = (index + 1) % b_size # modulo magic
      end
 
-    result == a_size
+    true 
 
   end
+
+  private 
+
+  def find_start_index(value, array)
+    index = 0
+    until value == array[index]
+      index += 1
+      return false if index > array.size 
+    end
+    index
+  end
+
 end
